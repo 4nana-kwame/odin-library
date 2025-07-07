94a2c9da614ecc77650b2ee0f3efbdf269752557
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-07-07 18:26:45 +0000

    Add Book constructor and library setup
    Initialized myLibrary array to store books.
    Created a Book constructor with title, author, number of pages, and read
    status properties. Also added an info() method to return a summary
    string.
    Implemented addBookToLibrary() to instatiate and add Book objects to the
    library array.

---
+++ script.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book(title, author, numOfPages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.readStatus = readStatus;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.numOfPages} pages, ${this.readStatus}.`;
+  }
+}
+
+function addBookToLibrary(title, author, numOfPages, readStatus) {
+  const book = new Book(title, author, numOfPages, readStatus);
+
+  myLibrary.push(book);
+}
