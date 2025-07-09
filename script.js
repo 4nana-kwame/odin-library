6f2c8d537d51ec8484116e90347c3c160f77f97e
Richmond Wiafe <gyebirichmond21@gmail.com>
2025-07-09 01:01:43 +0000

    Refactor displayBook() function
    Looped through myLibrary array and created HTML elemets to display the
    values of the objects in the array.
    Added sample books to be displayed when the page loads.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dialog = document.querySelector('#dialog');
 const addNewBook = document.querySelector('.add');
 const cancel = document.querySelector('.cancel');
+const booksContainer = document.querySelector('.books-container');
 const myLibrary = [];
 
 function Book(title, author, numOfPages, readStatus) {
@@ -22,7 +23,45 @@ function addBookToLibrary(title, author, numOfPages, readStatus) {
 
 function displayBook() {
   for (let book of myLibrary) {
-    
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book');
+    const bookTitle = document.createElement('h2');
+    bookTitle.classList.add('book-title');
+    const bookAuthor = document.createElement('p');
+    bookAuthor.classList.add('book-author');
+    const bookPages = document.createElement('p');
+    bookPages.classList.add('book-pages');
+    const btnContainer = document.createElement('div');
+    const readStatus = document.createElement('div');
+    btnContainer.classList.add('btn-container');
+    const toggleReadStatus = document.createElement('button');
+    toggleReadStatus.classList.add('toggle-read-status');
+    const removeBook = document.createElement('button');
+    removeBook.classList.add('remove-book');
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = `by ${book.author}`;
+    bookPages.textContent = `${book.numOfPages} pages`;
+    removeBook.textContent = 'Remove Book';
+
+    if (book.readStatus) {
+      readStatus.classList.add('read');
+      readStatus.textContent = ' Read';
+      toggleReadStatus.textContent = 'Mark as unread';
+    } else {
+      readStatus.classList.add('not-read');
+      readStatus.textContent = ' Not read';
+      toggleReadStatus.textContent = 'Mark as read';
+    }
+
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(readStatus);
+    btnContainer.appendChild(toggleReadStatus);
+    btnContainer.appendChild(removeBook);
+    bookCard.appendChild(btnContainer);
+    booksContainer.appendChild(bookCard);
   }
 }
 
@@ -34,3 +73,11 @@ cancel.addEventListener('click', (event) => {
   event.preventDefault();
   dialog.close();
 });
+
+// Sample books
+addBookToLibrary('The Way of Kings', 'Brandon Sanderson', 1093, true);
+addBookToLibrary('Red Rising', 'Pierce Brown', 353, false);
+addBookToLibrary('Shadows of self', 'Brandon Sanderson', 375, true);
+addBookToLibrary('The Sword of Kaigen', 'M.L. Wang', 339, false);
+
+displayBook();
